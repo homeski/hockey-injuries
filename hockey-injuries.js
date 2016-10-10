cc7258bbf7679a757c73780bdac7fdf72d02183c
Homero Pawlowski <homeski@cox.net>
2016-10-10 14:25:46 -0700

    data is now saved into redis but connection is not closed

--- hockey-injuries.js
+++ hockey-injuries.js
@@ -1,12 +1,20 @@
 var cheerio = require('cheerio');
 var fs = require('fs-extra');
+var redis = require("redis");
 var vsprintf = require('sprintf-js').vsprintf
 
+var client = redis.createClient({
+	host: '0.0.0.0'
+});
 var file = process.argv[2];
 
 var echo = function(str) {
 	console.log(str);
 }
+
+client.on("error", function (err) {
+	console.log("Error " + err);
+});
 
 var getPlayers = function(html) {
 	$ = cheerio.load(html);
@@ -43,15 +51,33 @@ var getPlayers = function(html) {
 	return list;
 }
 
+var insertPlayers = function(players, cb) {
+	for (var i = 0; i < players.length; i++) {
+		player = players[i];
+		key = 'player:' + player.id;
+
+		echo(vsprintf("searching: %s", [key]));
+		client.get(key, function(err, res) {
+			if (err) echo(err);
+			if (res == null) {
+				echo(vsprintf('set: %s %s', [this.playerKey, this.playerName]));
+				client.set(this.playerKey, this.playerName, function(err, result) {
+					echo(err);
+					echo(result);
+				});
+			}
+		}.bind({
+			playerKey: key,
+			playerName: player.name
+		}));
+	}
+};
+
 fs.readFile(file, (err, data) => {
 	if (err) throw err;
 
 	var players = getPlayers(data.toString());
-
-	for (var i = 0; i < players.length; i++) {
-		player = players[i];
-		echo(vsprintf("%s %s %s", [player.name, player.id, player.hash]));
-		echo(player);
-	}
+	insertPlayers(players);
+	
 });
 
