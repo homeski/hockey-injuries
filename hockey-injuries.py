6a9a1c44efbb50bbd451197453d17f8da18b636e
Homero Pawlowski <homeski@cox.net>
2016-10-09 22:07:43 -0700

    Created shell script to download injury page

---
+++ hockey-injuries.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+from HTMLParser import HTMLParser
+
+print 'foo'
---
+++ hockey-injuries.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+verbose=0
+
+if [[ $1 == '-v' ]]; then
+  verbose=1
+fi
+
+DATE=$(date +%Y-%m-%d)
+TIME=$(date +%H-%M)
+
+if [ $verbose == 1 ]; then echo $DATE; fi
+if [ $verbose == 1 ]; then echo $TIME; fi
+
+FOLDER=./download/${DATE}
+
+mkdir -p $FOLDER
+
+wget http://www.rotoworld.com/teams/injuries/nhl/all/ -O ${FOLDER}/injuries-${TIME}.html
