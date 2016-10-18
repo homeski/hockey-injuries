f519bb18c66beabf50acbb1f69a933329bf2fa6c
Homero Pawlowski <homeski@cox.net>
2016-10-18 16:59:07 -0700

    Changed sh script to pass thru arguments to node script

--- hockey-injuries.sh
+++ hockey-injuries.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-verbose=0
-
-if [[ $1 == '-v' ]]; then
-  verbose=1
-fi
-
 DATE=$(date +%Y-%m-%d)
 TIME=$(date +%H-%M)
-
-if [ $verbose == 1 ]; then echo $DATE; fi
-if [ $verbose == 1 ]; then echo $TIME; fi
 
 FOLDER=./download/${DATE}
 FILE=${FOLDER}/injuries-${TIME}.html
@@ -19,4 +10,4 @@ mkdir -p $FOLDER
 
 wget http://www.rotoworld.com/teams/injuries/nhl/all/ -O $FILE
 
-node hockey-injuries.js $FILE
+node hockey-injuries.js $FILE $1
